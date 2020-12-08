9ecbae67b21221fe3c325056faa31f53c4a94438
elentuuk <s21910086@student.unklab.ac.id>
2020-12-08 13:45:37 +0800

    Final Project

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ function helloProfile(){
     document.querySelector(".award").style.fontSize= "3.5em";
     document.querySelector(".kontak").style.fontSize= "3.5em";
 
-    document.querySelector(".bodies").style.backgroundColor= "#ebebeb";
+    document.querySelector(".biografi").style.backgroundColor = "#839b97";
+    document.querySelector(".edukasi").style.backgroundColor= "#839b97";
+    document.querySelector(".award").style.backgroundColor= "#839b97";
+    document.querySelector(".kontak").style.backgroundColor= "#839b97";
 
+    document.querySelector(".bodies").style.backgroundColor= "#ebebeb";
+    document.getElementById("images").src= "elen-tuuk.png";
+}
+
+function displayDate(){
+    document.getElementById("time").innerHTML = Date();
 }
